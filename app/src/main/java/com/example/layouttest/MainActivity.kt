9697fee6cc6319b25a82672ffad3282ee57a8522
superDongE <dlagoehd13@gmail.com>
2021-12-25 23:23:22 +0900

    테스트 완료

--- app/src/main/java/com/example/layouttest/MainActivity.kt
+++ app/src/main/java/com/example/layouttest/MainActivity.kt
@@ -20,81 +20,77 @@ import java.lang.Math.abs
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    var scrollType: Boolean = true
+    var scrollType: Boolean = false // scroll status
+    var tabType : Boolean = false // tab click status
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        Log.d("superDOngE", "onCreate() run")
+
         binding.mainTab.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+
             @SuppressLint("ClickableViewAccessibility")
             override fun onTabSelected(tab: TabLayout.Tab?) {
                 // 탭 상태가 선택 상태로 변경
-                Log.d("dlagoehd13", "position: ${tab?.position}")
+                Log.d("superDOngE", "position: ${tab?.position}")
+                tabType = true
                 when (tab?.position) {
                     0 -> {
 //                        scrollToRow(bind.mainScroll, bind.mainLinear, bind.tv1)
                         binding.mianScroll.smoothScrollToView(binding.tv1)
+                        tabType = false
                     }
                     1 -> {
 //                        scrollToRow(bind.mainScroll, bind.mainLinear, bind.tv2)
                         binding.mianScroll.smoothScrollToView(binding.tv2)
+                        tabType = false
                     }
 
                     2 -> {
 //                        scrollToRow(bind.mainScroll, bind.mainLinear, bind.tv3)
                         binding.mianScroll.smoothScrollToView(binding.tv3)
+                        tabType = false
                     }
 
                     3 -> {
 //                        scrollToRow(bind.mainScroll, bind.mainLinear, bind.tv4)
                         binding.mianScroll.smoothScrollToView(binding.tv4)
+                        tabType = false
                     }
 
                     4 -> {
 //                        scrollToRow(bind.mainScroll, bind.mainLinear, bind.tv5)
                         binding.mianScroll.smoothScrollToView(binding.tv5)
+                        tabType = false
                     }
 
                     5 -> {
 //                        scrollToRow(bind.mainScroll, bind.mainLinear, bind.tv6)
                         binding.mianScroll.smoothScrollToView(binding.tv6)
+                        tabType = false
                     }
 
                     else -> {
 //                        scrollToRow(bind.mainScroll, bind.mainLinear, bind.tv7)
                         binding.mianScroll.smoothScrollToView(binding.tv7)
+                        tabType = false
                     }
                 }
 
 
                 // touch listener
-                binding.mianScroll.setOnTouchListener { v, event ->
+                binding.mianScroll.setOnTouchListener { _, event ->
                     when (event?.action) {
-//                MotionEvent.ACTION_SCROLL -> {
-//                    Log.d("superDOngE", "ACTION_SCROLL")
-//                    false
-//                }
-//                MotionEvent.ACTION_MOVE -> {
-//                    Log.d("superDOngE", "ACTION_MOVE")
-//                    false
-//                }
-//
-//                MotionEvent.ACTION_DOWN -> {
-//                    Log.d("superDOngE", "ACTION_DOWN")
-//                    false
-//                }
-//
-//                MotionEvent.ACTION_CANCEL -> {
-//                    Log.d("superDOngE", "ACTION_CANCEL")
-//                    false
-//                }
-
                         // Motion active false
                         // 모션 동작이 마무리되어 손가락이 사용자를 떠남
                         MotionEvent.ACTION_UP -> {
                             Log.d("superDOngE", "ACTION_UP")
-                            scrollType = true
+                            val handler = Handler(mainLooper)
+                            handler.postDelayed(
+                                {scrollType = true}, 400
+                            )
                             false
                         }
                         else -> {
@@ -110,17 +106,16 @@ class MainActivity : AppCompatActivity() {
                 // 스크롤 리스너를 달아준다.
                 binding.mianScroll.viewTreeObserver.addOnScrollChangedListener {
                     val scrollY = binding.mianScroll.scrollY // Y value
+                    Log.d("superDOngE", "scrollType : $scrollType, tabType: $tabType")
 
-                    val handler = Handler(mainLooper)
-
-                    if (!scrollType) {
+                    if (!scrollType && !tabType) {
                         when (scrollY) {
-                            in binding.tv1.top .. binding.tv1.bottom -> {
+                            in binding.tv1.top..binding.tv1.bottom -> {
                                 binding.mainTab.getTabAt(0)?.select()
 //                                binding.mainTab.getTabAt(0)?.select()
                                 //                        bind.mainTab.getTabAt(0)?.isSelected
                             }
-                            in binding.tv2.top .. binding.tv2.bottom -> {
+                            in binding.tv2.top..binding.tv2.bottom -> {
                                 binding.mainTab.getTabAt(1)?.select()
                                 //                        bind.mainTab.getTabAt(1)?.isSelected
                             }
@@ -203,5 +198,44 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         })
+    }
+
+
+    private fun checkPostion(scrollY: Int) {
+
+        when (scrollY) {
+            in binding.tv1.top..binding.tv1.bottom -> {
+                binding.mainTab.getTabAt(0)?.select()
+//                                binding.mainTab.getTabAt(0)?.select()
+                //                        bind.mainTab.getTabAt(0)?.isSelected
+            }
+            in binding.tv2.top..binding.tv2.bottom -> {
+                binding.mainTab.getTabAt(1)?.select()
+                //                        bind.mainTab.getTabAt(1)?.isSelected
+            }
+            in binding.tv3.top..binding.tv3.bottom -> {
+                binding.mainTab.getTabAt(2)?.select()
+                //                        bind.mainTab.getTabAt(2)?.isSelected
+            }
+            in binding.tv4.top..binding.tv4.bottom -> {
+                binding.mainTab.getTabAt(3)?.select()
+                //                        bind.mainTab.getTabAt(3)?.isSelected
+            }
+            in binding.tv5.top..binding.tv5.bottom -> {
+                binding.mainTab.getTabAt(4)?.select()
+                //                        bind.mainTab.getTabAt(4)?.isSelected
+            }
+            in binding.tv6.top..binding.tv6.bottom -> {
+                binding.mainTab.getTabAt(5)?.select()
+                //                        bind.mainTab.getTabAt(5)?.isSelected
+            }
+            in binding.tv7.top..binding.tv7.bottom -> {
+                binding.mainTab.getTabAt(6)?.select()
+                //                        bind.mainTab.getTabAt(6)?.isSelected
+            }
+            else -> {
+                Log.d("t", "not Working")
+            }
+        }
     }
 }
